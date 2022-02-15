502dee390746d93e0cdfe691a26e30b954437a8c
NIKITARYDAKOV <inikita0907@gmail.com>
2022-02-15 09:16:56 +0300

    part2

--- components/header/header.js
+++ components/header/header.js
@@ -4,7 +4,7 @@ class Basket_head {
     render(count) {
         const html = ` 
 
-            <div class="img_basket" href="../html/basket_html.html">
+            <div class="img_basket" onclick='location.href="../html/basket_html.html"'>
             <img src="../img/basket.png"  >
             <text placeholder=''>${count}</text>
 </div>
@@ -23,6 +23,7 @@ const pcStore = localStorageUtil_PC.getPC();
 const tehnicStore = localStorageUtil_TEHNIC.getTehnic();
 const phoneStore = localStorageUtil_PHONE.getPhone();
 const instrumentsStore = localStorageUtil_INSTRUMENTS.getInstruments();
+
 const header_basket = new Basket_head();
 const cou = pcStore.length + tehnicStore.length + phoneStore.length + instrumentsStore.length;
 header_basket.render(cou);
