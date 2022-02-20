8fb1d50df77ae86de8f16d598f1875586eedec5d
NIKITARYDAKOV <inikita0907@gmail.com>
2022-02-20 19:24:10 +0300

    part2

--- components/basket/basket_html.js
+++ components/basket/basket_html.js
@@ -3,72 +3,68 @@ class Basket_catalog {
 
     // Если ввели промокод, и проверка на ранее введеный промо
     Promokod() {
-        const getSell = LocalStorageUtil_SELL.getSell();
-        const promo = LocalStorageUtil_PROMO.getPromo();
-        allSum = getSell[0];
         let input = document.getElementById("input_promo").value;
         let text = document.getElementById("text_sum");
+        const getPromo = LocalStorageUtilSUM.getPromo();
+        if(getPromo == 0){
         if (input == "SELL") {
-            if (promo.length == 0) {
-                if (promo[0] !== 'done') {
-                    let promoDone = "done";
-                    allSum = allSum - (allSum * 0.3);
-                    text.textContent = "Общая стоимость: " + allSum.toFixed(0) + " Р";
-                    const pushSell = LocalStorageUtil_SELL.putSell(allSum);
-                    const pushRemove = LocalStorageUtil_SELL.removeSell(allSum);
-                    const push_promo = LocalStorageUtil_PROMO.putPromo(promoDone);
-                }
-                else {
-                    alert("Вы уже ввели промокод");
-                }
-            }
+            const pushSum = LocalStorageUtilSUM.pushSum(input);
         }
-
+    }else {alert("Вы уже ввели промокод");}
+location.reload();
     }
 
     // Методы для удаления определеного элемента из корзины
-    Set_remove_pc(element, id,price) {
+    Set_remove_pc(element, id, price) {
         let name = 'pc';
-        const removePc =  LocalStorageUtilPRODUCTS.putProducts(name,id,price,0);
+        const removePc = LocalStorageUtilPRODUCTS.putProducts(name, id, price, 0);
         location.reload();
     }
-    Set_remove_phone(element, id) {
-        const pushPhone = localStorageUtil_PHONE.putPhone(id);
-        var d = document.getElementById("all_content_basket");
-        var d_nested = document.getElementById("basket_modal2");
-        var throwawayNode = d.removeChild(d_nested);
+    Set_remove_phone(element, id, price) {
+        let name = 'phone';
+        const removePc = LocalStorageUtilPRODUCTS.putProducts(name, id, price, 0);
         location.reload();
     }
-    Set_remove_tehnic(element, id) {
-        const pushTehnic = localStorageUtil_TEHNIC.putTehnic(id);
-        var d = document.getElementById("all_content_basket");
-        var d_nested = document.getElementById("basket_modal3");
-        var throwawayNode = d.removeChild(d_nested);
+    Set_remove_tehnic(element, id, price) {
+        let name = 'tehnic';
+        const removePc = LocalStorageUtilPRODUCTS.putProducts(name, id, price, 0);
         location.reload();
     }
-    Set_remove_instrum(element, id) {
-        const pushInstruments = localStorageUtil_INSTRUMENTS.putInstruments(id);
-        var d = document.getElementById("all_content_basket");
-        var d_nested = document.getElementById("basket_modal4");
-        var throwawayNode = d.removeChild(d_nested);
+    Set_remove_instrum(element, id, price) {
+        let name = 'instruments';
+        const removePc = LocalStorageUtilPRODUCTS.putProducts(name, id, price, 0);
         location.reload();
     }
 
-
+    LoadSum() {
+        let getPc, getPhone, getTehnic, getInstr;
+        const pcSum = LocalStorageUtilSUM.getSumPc(); if (pcSum !== 0) { getPc = pcSum.sum; } else getPc = 0;
+        const phoneSum = LocalStorageUtilSUM.getSumPhone(); if (phoneSum !== 0) { getPhone = phoneSum.sum; } else getPhone = 0;
+        const tehnicSum = LocalStorageUtilSUM.getSumTehnic(); if (tehnicSum !== 0) { getTehnic = tehnicSum.sum; } else getTehnic = 0;
+        const InstrSum = LocalStorageUtilSUM.getSumInstr(); if (InstrSum !== 0) { getInstr = InstrSum.sum; } else getInstr = 0;
+        let allSum = getPc + getPhone + getTehnic + getInstr;
+        if(allSum<0){
+            allSum = 0;
+        }
+        return allSum;
+    }
 
     render() {
 
         // Объявление всех переменных из локального хранилища
         const activeClass_basket = ' ';
-        const productsStore = localStorage.getItem('productsPc');
+        const productsStore = LocalStorageUtilPRODUCTS.getProductsPc();
+        const productsStorePhone = LocalStorageUtilPRODUCTS.getProductsPhone();
+        const productsStoreTehnic = LocalStorageUtilPRODUCTS.getProductsTehnic();
+        const productsStoreInstr = LocalStorageUtilPRODUCTS.getProductsInstr();
 
         let html_catalog_basket = '';
 
         // Отрисовка элементов ,которые выбрал покупатель в корзину
         CATALOG.forEach(({ id, img, text, price }) => {
-            if(productsStore !== null){
-            if (productsStore.indexOf(id) !== -1) {
-                html_catalog_basket += `
+            if (productsStore !== null) {
+                if (productsStore.indexOf(id) !== -1) {
+                    html_catalog_basket += `
                 <div class="basket_modal" id="basket_modal">
                 <img class="basket_modal_img" src="${img}">
                 <div class="basket_modal_text">${text}</div>
@@ -78,147 +74,164 @@ class Basket_catalog {
             </div>
             </div>
 `;
+                }
             }
-        }
         });
+
+
         CATALOG_PHONE.forEach(({ id, img, text, price }) => {
-            if (productsStore.indexOf(id) !== -1) {
-                html_catalog_basket += `
+            if (productsStorePhone !== null) {
+                if (productsStorePhone.indexOf(id) !== -1) {
+                    html_catalog_basket += `
                 <div class="basket_modal" id="basket_modal2">
                 <img class="basket_modal_img" src="${img}">
                 <div class="basket_modal_text">${text}</div>
                 <div class="basket_modal_pib_phone">
                     <div class="basket_modal_price">${price.toLocaleString()} Р</div>
-                    <button class="basket_modal_button ${activeClass_basket}" onclick="basketStore_page.Set_remove_phone(this,'${id}');">Удалить</button>
+                    <button class="basket_modal_button ${activeClass_basket}" onclick="basketStore_page.Set_remove_phone(this,'${id}','${price}');">Удалить</button>
             </div>
             </div>
 `;
+                }
             }
         });
+
+
         CATALOG_TEHNIC.forEach(({ id, img, text, price }) => {
-            if (productsStore.indexOf(id) !== -1) {
-                html_catalog_basket += `
+            if (productsStoreTehnic !== null) {
+                if (productsStoreTehnic.indexOf(id) !== -1) {
+                    html_catalog_basket += `
                 <div class="basket_modal" id="basket_modal3">
                 <img class="basket_modal_img" src="${img}">
                 <div class="basket_modal_text">${text}</div>
                 <div class="basket_modal_pib">
                     <div class="basket_modal_price">${price.toLocaleString()} Р</div>
-                    <button class="basket_modal_button ${activeClass_basket}" onclick="basketStore_page.Set_remove_tehnic(this,'${id}');">Удалить</button>
+                    <button class="basket_modal_button ${activeClass_basket}" onclick="basketStore_page.Set_remove_tehnic(this,'${id}','${price}');">Удалить</button>
             </div>
             </div>
 `;
+                }
             }
         });
+
+
         CATALOG_INSTRUMENTS.forEach(({ id, img, text, price }) => {
-            if (productsStore.indexOf(id) !== -1) {
-                html_catalog_basket += `
+            if (productsStoreInstr !== null) {
+                if (productsStoreInstr.indexOf(id) !== -1) {
+                    html_catalog_basket += `
                 <div class="basket_modal" id="basket_modal4">
                 <img class="basket_modal_img" src="${img}">
                 <div class="basket_modal_text">${text}</div>
                 <div class="basket_modal_pib">
                     <div class="basket_modal_price">${price.toLocaleString()} Р</div>
-                    <button class="basket_modal_button ${activeClass_basket}" onclick="basketStore_page.Set_remove_instrum(this,'${id}');">Удалить</button>
+                    <button class="basket_modal_button ${activeClass_basket}" onclick="basketStore_page.Set_remove_instrum(this,'${id}','${price}');">Удалить</button>
             </div>
             </div>
 `;
+                }
             }
         });
 
-
-
-
+        const allSum = this.LoadSum();
         // Статическая отрисовка сайта
         const html = `
-        <div class="all_content_basket" id="all_content_basket" style="display: flex">
-        ${html_catalog_basket}
-        </div>
-        `;
+            <div class="all_content_basket" id="all_content_basket" style="display: flex">
+                 ${html_catalog_basket}
+                    </div>`;
 
         const div = document.createElement('div');
-
         div.classList.add('content_basket')
-
         const body = document.body;
         body.appendChild(div);
 
-
-        const img_menu = `
-<div class="img_menu">
-<img src="../img/menu.svg">
-        </div>`;
-        const header = `
-<div class="header">
-<div class="header_ic">
-    <img class="logo_img" src="../img/karmy.svg">
-    <button class="bt_login">Login</button>
-</div>
-<div class="double_header">
-    <div class="head_nav">
-        <ul class="nav">
-            <li class="nav-item">
-                <a class="nav-link_home" href="../html/index.html">Главная</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link_srav" href="#">Сравнить</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link_back" href="../html/basket_html.html">Корзина</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link_shop" href="#">Магазин</a>
-            </li>
-        </ul>
+        const contentHtml = () => {
+            const img_menu = `
+    <div class="img_menu">
+        <img src="../img/menu.svg">
+          </div>`;
+            const header = `
+  <div class="header">
+        <div class="header_ic">
+              <img class="logo_img" src="../img/karmy.svg">
+                 <button class="bt_login">Login</button>
+                    </div>
+        <div class="double_header">
+             <div class="head_nav">
+                   <ul class="nav">
+                        <li class="nav-item">
+                          <a class="nav-link_home" href="../html/index.html">Главная</a>
+                        </li>
+                        <li class="nav-item">
+                         <a class="nav-link_srav" href="#">Сравнить</a>
+                        </li>
+                        <li class="nav-item">
+                        <a class="nav-link_back" href="../html/basket_html.html">Корзина</a>
+                        </li>
+                        <li class="nav-item">
+                         <a class="nav-link_shop" href="#">Магазин</a>
+                        </li>
+                   </ul>
+        </div>
     </div>
-</div>
-        </div>`;
-        const left_cen = `
-            <div class="left_cen">
-        <div class="comp_content_basket">
-        <div class="adaptiv">
-        <div class="all_content ">
-        ${html}
-         <div>
-        <input placeholder="Введите промокод" class="input_promo" id="input_promo">
-        <button class="bt_promo" onclick="basketStore_page.Promokod()">ОК</button>
-        </div>
-        <div class="sell_basket">
-        <text class="text_sum" id="text_sum">Общая стоимость: Р</text>
-        <button class="bt_buy">Купить</button>
+  </div>`;
+            const left_cen = `
+        <div class="left_cen">
+            <div class="comp_content_basket" id="comp_content_basket">
+              <div class="adaptiv">
+                  <div class="all_content" id="all_content">
+                      ${html}
+                  <div>
+                    <input placeholder="Введите промокод" class="input_promo" id="input_promo">
+                    <button class="bt_promo" onclick="basketStore_page.Promokod()">ОК</button>
+                  </div>
+                 <div class="sell_basket">
+                    <text class="text_sum" id="text_sum">Общая стоимость: ${allSum}  Р</text>
+                    <button class="bt_buy">Купить</button>
+                 </div>
+               </div>
             </div>
-            </div>
-        </div>
         </div>`;
-        const footer = `<footer class="foot_er">
-
+            const footer = `
+    <footer class="foot_er">
 <ul class="nav_bot">
-    <li class="nav-item_bot">
-        <a class="nav-link_about" href="#">О нас</a>
-    </li>
-    <li class="nav-item_bot">
-        <a class="nav-link_vac" href="#">Вакансии</a>
-    </li>
-    <li class="nav-item_bot">
-        <a class="nav-link_back" href="#">Корзина</a>
-    </li>
-    <li class="nav-item_bot">
-        <a class="nav-link_help" href="#">Помощь</a>
-    </li>
+<li class="nav-item_bot">
+    <a class="nav-link_about" href="#">О нас</a>
+</li>
+<li class="nav-item_bot">
+    <a class="nav-link_vac" href="#">Вакансии</a>
+</li>
+<li class="nav-item_bot">
+    <a class="nav-link_back" href="#">Корзина</a>
+</li>
+<li class="nav-item_bot">
+    <a class="nav-link_help" href="#">Помощь</a>
+</li>
 </ul>
 <div class="email_by_t">
-    <input class="in_email" placeholder="Enter email.."></input>
-    <button class="bt_email">Submit</button>
+<input class="in_email" placeholder="Enter email.."></input>
+<button class="bt_email">Submit</button>
 </div>
 <div class="Copyright">Copyright Karmy</div>
-        </footer>`;
+    </footer>`;
+            const basket_null = `
+    <div id="basket_text" class="basket_text" style="display:none;margin:auto;"><text ">Ваша корзина пуста</text></div>
+    `;
+            const content_html = `
+    ${img_menu}
+    ${header}
+    ${left_cen}
+    ${basket_null}
+    ${footer}
+    `;
+            return content_html;
+        }
 
-        const content_html = `
-        ${img_menu}
-        ${header}
-        ${left_cen}
-        ${footer}
-        `;
-
-        div.innerHTML = content_html;
+        div.innerHTML = contentHtml();
+        if (allSum == 0) {
+            document.getElementById('all_content').style.display = "none";
+            document.getElementById('comp_content_basket').style.display = "none";
+            document.getElementById('basket_text').style.display = "block";
+        }
     }
 }
 // Отрисовка класса
--- utils/localStorage.js
+++ utils/localStorage.js
@@ -57,26 +57,27 @@ class LocalStorageUtil_products {
 
                 const countSum = () => {
                     sum = JSON.parse(localStorage.getItem('sumPc') || '0');
-                    if (sum == 0) { sum = 0; } else { sum = Number(sum.sum); }
+                    if (sum != 0) { sum = Number(sum.sum); }
                     sum += price * count;
-                    const sumPc = { sum: sum }
+                    const sumPc = { sum: sum}
                     localStorage.setItem('sumPc', JSON.stringify(sumPc));
                 }
 
                 products.push(id);
                 countSum();
-                
-            }else{
+
+            } else {
                 const removeSum = () => {
                     sum = JSON.parse(localStorage.getItem('sumPc') || '0');
                     if (sum == 0) { sum = 0; } else { sum = Number(sum.sum); }
-                    sum = sum -price;
+                    sum = sum - price;
                     const sumPc = { sum: sum }
                     localStorage.setItem('sumPc', JSON.stringify(sumPc));
                 }
                 products.splice(index, 1);
                 removeSum();
             }
+
             localStorage.setItem(this.NamePc, JSON.stringify(products));
             return { pushStore }
         }
@@ -89,17 +90,26 @@ class LocalStorageUtil_products {
             if (index == -1) {
                 pushStorePhone = true;
 
-
                 const countSum = () => {
                     sum = JSON.parse(localStorage.getItem('sumPhone') || '0');
                     if (sum == 0) { sum = 0; } else { sum = Number(sum.sum); }
                     sum += price * count;
                     const sumPhone = { sum: sum }
-                    return sumPhone;
+                    localStorage.setItem('sumPhone', JSON.stringify(sumPhone));
                 }
 
                 productsPhone.push(id);
-                localStorage.setItem('sumPhone', JSON.stringify(countSum()));
+                countSum();
+            } else {
+                const removeSum = () => {
+                    sum = JSON.parse(localStorage.getItem('sumPhone') || '0');
+                    if (sum == 0) { sum = 0; } else { sum = Number(sum.sum); }
+                    sum = sum - price;
+                    const sumPc = { sum: sum }
+                    localStorage.setItem('sumPhone', JSON.stringify(sumPc));
+                }
+                productsPhone.splice(index, 1);
+                removeSum();
             }
             localStorage.setItem(this.NamePhone, JSON.stringify(productsPhone));
             return { pushStorePhone }
@@ -118,11 +128,22 @@ class LocalStorageUtil_products {
                     if (sum == 0) { sum = 0; } else { sum = Number(sum.sum); }
                     sum += price * count;
                     const sumTehnic = { sum: sum }
-                    return sumTehnic;
+                    localStorage.setItem('sumTehnic', JSON.stringify(sumTehnic));
                 }
 
                 productsTehnic.push(id);
-                localStorage.setItem('sumTehnic', JSON.stringify(countSum()));
+                countSum();
+
+            } else {
+                const removeSum = () => {
+                    sum = JSON.parse(localStorage.getItem('sumTehnic') || '0');
+                    if (sum == 0) { sum = 0; } else { sum = Number(sum.sum); }
+                    sum = sum - price;
+                    const sumPc = { sum: sum }
+                    localStorage.setItem('sumTehnic', JSON.stringify(sumPc));
+                }
+                productsTehnic.splice(index, 1);
+                removeSum();
             }
             localStorage.setItem(this.NameTehnic, JSON.stringify(productsTehnic));
             return { pushStoreTehnic }
@@ -141,16 +162,100 @@ class LocalStorageUtil_products {
                     if (sum == 0) { sum = 0; } else { sum = Number(sum.sum); }
                     sum += price * count;
                     const sumInstr = { sum: sum }
-                    return sumInstr;
+                    localStorage.setItem('sumInstr', JSON.stringify(sumInstr));
                 }
 
                 productsInstr.push(id);
-                localStorage.setItem('sumInstr', JSON.stringify(countSum()));
+                countSum();
+            } else {
+                const removeSum = () => {
+                    sum = JSON.parse(localStorage.getItem('sumInstr') || '0');
+                    if (sum == 0) { sum = 0; } else { sum = Number(sum.sum); }
+                    sum = sum - price;
+                    const sumPc = { sum: sum }
+                    localStorage.setItem('sumInstr', JSON.stringify(sumPc));
+                }
+                productsInstr.splice(index, 1);
+                removeSum();
             }
             localStorage.setItem(this.NameInstruments, JSON.stringify(productsInstr));
             return { pushStoreInstr }
         }
     }
 }
+class LocalStorageUtil_sumCount{
+    constructor(){
+        this.pcSum = 'sumPc';
+        this.phoneSum = 'sumPhone';
+        this.tehnicSum = 'sumTehnic';
+        this.instrSum = 'sumInstr';
+    }
+    getSumPc() {
+        const pcLocalStorage = localStorage.getItem(this.pcSum);
+        if (pcLocalStorage !== null) {
+            return JSON.parse(pcLocalStorage);
+        }
+        else
+            return 0;
+    }
+    getSumPhone() {
+        const pcLocalStorage = localStorage.getItem(this.phoneSum);
+        if (pcLocalStorage !== null) {
+            return JSON.parse(pcLocalStorage);
+        }
+        else
+            return 0;
+    }
+    getSumTehnic() {
+        const pcLocalStorage = localStorage.getItem(this.tehnicSum);
+        if (pcLocalStorage !== null) {
+            return JSON.parse(pcLocalStorage);
+        }
+        else
+            return 0;
+    }
+    getSumInstr() {
+        const pcLocalStorage = localStorage.getItem(this.instrSum);
+        if (pcLocalStorage !== null) {
+            return JSON.parse(pcLocalStorage);
+        }
+        else
+            return 0;
+    }
+    getPromo() {
+        const pcLocalStorage = localStorage.getItem('promo');
+        if (pcLocalStorage !== null) {
+            return pcLocalStorage;
+        }
+        else
+            return 0;
+    }
+    
+    pushSum(name){
+        let sumPc = this.getSumPc();
+        let sumPhone = this.getSumPhone();
+        let sumTehnic = this.getSumTehnic();
+        let sumInstr = this.getSumInstr();
+        if(sumPc!=0){sumPc = Number(sumPc.sum); sumPc -= (sumPc * 0.3);}
+        if(sumPhone!=0){sumPhone = Number(sumPhone.sum); sumPhone -= sumPhone * 0,2;}
+        if(sumTehnic!=0){sumTehnic = Number(sumTehnic.sum); sumTehnic -= sumTehnic * 0,1;}
+        if(sumInstr!=0){sumInstr = Number(sumInstr.sum); sumInstr -= sumInstr * 0,25;}
+
+        const sumPc2 = { sum: sumPc}
+        localStorage.setItem('sumPc', JSON.stringify(sumPc2));
+
+        const sumPhone2 = { sum: sumPhone}
+        localStorage.setItem('sumPhone', JSON.stringify(sumPhone2));
+
+        const sumTehnic2 = { sum: sumTehnic}
+        localStorage.setItem('sumTehnic', JSON.stringify(sumTehnic2));
+
+        const sumInstr2 = { sum: sumInstr}
+        localStorage.setItem('sumInstr', JSON.stringify(sumInstr2));
+
+        const pushPromo = localStorage.setItem('promo',name);
+    }
+}
 
 const LocalStorageUtilPRODUCTS = new LocalStorageUtil_products();
+const LocalStorageUtilSUM = new LocalStorageUtil_sumCount();
